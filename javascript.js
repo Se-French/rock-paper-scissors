3f6c542093b061b245c1fac757cf68b6098ccee2
Se-French <116462983+Se-French@users.noreply.github.com>
2022-12-29 15:55:48 -0500

    Create playerSelection
    Create playerSelection for game using a prompt to
    get input from the user. Use an else if statement
    to account for the cancel button, incorrect spelling
    or options not listed, and to make the playerSelection
    case-insensitive.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,34 @@
-const options = ['rock', 'paper', 'scissors']; 
+const options = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice(options){
     let randomChoice = options[Math.floor(Math.random() * options.length)];
     return randomChoice;
 }
-//This function will randomly return 'rock', 'paper', or 'scissors'
+//This function (above) will randomly return 'rock', 'paper', or 'scissors'
+//Use this for computerSelection
 
  console.log (getComputerChoice(options));
+
+//Below is where we get the value for playerSelection
+ 
+ const playerChoice = prompt("Choose rock, paper, or scissors!");
+
+if (playerChoice === null){
+    playerSelection = 'Invalid Option';
+}else if (playerChoice.toLowerCase() === 'rock'){
+    playerSelection = 'rock';
+}else if (playerChoice.toLowerCase() === 'paper'){
+    playerSelection = 'paper';
+}else if (playerChoice.toLowerCase() === 'scissors'){
+    playerSelection = 'scissors';
+}else{
+    playerSelection = 'Invalid Option';
+}    
+
+console.log (playerSelection);
+
+//Using the above else...if statement makes the case 
+//of the users input not matter, since the computer 
+//uses all lower case options for its Selection. It 
+//also makes all other values, including the cancel 
+//button, invalid options for the game.
