dff10f275f9c9962e01ade231630450b1bcbd9f3
Se-French <116462983+Se-French@users.noreply.github.com>
2022-12-29 16:50:51 -0500

    Create function to play a round of the game
    Create a funciton called playRound to play a round of the game and return
    the results for all possible outcomes.

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function getComputerChoice(options){
 //Use this for computerSelection
 
 const computerSelection = getComputerChoice(options);
-console.log(computerSelection);
+//console.log(computerSelection);
 
 //Below is where we get the value for playerSelection
  
@@ -26,11 +26,36 @@ if (playerChoice === null){
     playerSelection = 'Invalid Option';
 }    
 
-console.log (playerSelection);
+//console.log (playerSelection);
 
-//Using the above else...if statement makes the case 
-//of the users input not matter, since the computer 
-//uses all lower case options for its Selection. It 
-//also makes all other values, including the cancel 
-//button, invalid options for the game.
+//Using the above else...if statement makes the case of the users input not matter, 
+//since the computer uses all lower case options for its Selection. It also makes 
+//all other values, including the cancel button, invalid options for the game.
 
+
+//The following function plays a single round of the game and includes results
+//for all possible outcomes 
+
+function playRound(playerSelection, computerSelection){
+        if (playerSelection === 'rock' && computerSelection === 'scissors'){
+            return 'You win! Rock beats Scissors';
+        } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+            return 'You win! Scissors beats Paper';
+        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+            return 'You win! Paper beats Rock';
+        } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+            return 'You lose! Rock beats Scissors';
+        } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+            return 'You lose! Paper beats Rock';
+        } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+            return 'You lose! Scissors beats Paper';
+        } else if ((playerSelection === 'paper' && computerSelection === 'paper')
+                || (playerSelection === 'rock' && computerSelection === 'rock')
+                || (playerSelection === 'scissors' && computerSelection === 'scissors')){
+            return 'Wow, it\'s a tie!';
+        } else {
+            return 'Sorry, Invalid Option!';  
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
