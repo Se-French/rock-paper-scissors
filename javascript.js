8fbc7de2839983068224bb4437f3a72e5c0ac00b
Se-French <116462983+Se-French@users.noreply.github.com>
2022-12-30 16:01:04 -0500

    Create game function
    Create game function to run playRound() 5 times, keep score, and declare a winner
    at the end of the 5 rounds. Commented out previously stated global variables for
    scope reasons. Changed playRound() returns to include "this round".

--- javascript.js
+++ javascript.js
@@ -7,24 +7,28 @@ function getComputerChoice(){
     return randomChoice;
 }
 
-const computerSelection = getComputerChoice();
+//let computerSelection = getComputerChoice();
 //console.log(computerSelection);
 
 //Below is where we get the value for playerSelection
- 
- const playerChoice = prompt("Choose rock, paper, or scissors!");
 
-if (playerChoice === null){
-    playerSelection = 'Invalid Option';
-}else if (playerChoice.toLowerCase() === 'rock'){
-    playerSelection = 'rock';
-}else if (playerChoice.toLowerCase() === 'paper'){
-    playerSelection = 'paper';
-}else if (playerChoice.toLowerCase() === 'scissors'){
-    playerSelection = 'scissors';
-}else{
-    playerSelection = 'Invalid Option';
-}    
+function getPlayerChoice(){
+    const playerChoice = prompt("Choose rock, paper, or scissors!");
+
+    if (playerChoice === null){
+        return 'Invalid Option';
+    }else if (playerChoice.toLowerCase() === 'rock'){
+        return 'rock';
+    }else if (playerChoice.toLowerCase() === 'paper'){
+        return 'paper';
+    }else if (playerChoice.toLowerCase() === 'scissors'){
+        return 'scissors';
+    }else{
+        return 'Invalid Option';
+        }  
+    }
+
+//let playerSelection = getPlayerChoice();
 
 //console.log (playerSelection);
 
@@ -33,24 +37,68 @@ if (playerChoice === null){
 
 function playRound(playerSelection, computerSelection){
         if (playerSelection === 'rock' && computerSelection === 'scissors'){
-            return 'You win! Rock beats Scissors';
+            return 'You win this round! Rock beats Scissors';
         } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-            return 'You win! Scissors beats Paper';
+            return 'You win this round! Scissors beats Paper';
         } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-            return 'You win! Paper beats Rock';
+            return 'You win this round! Paper beats Rock';
         } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-            return 'You lose! Rock beats Scissors';
+            return 'You lose this round! Rock beats Scissors';
         } else if (playerSelection === 'rock' && computerSelection === 'paper'){
-            return 'You lose! Paper beats Rock';
+            return 'You lose this round! Paper beats Rock';
         } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-            return 'You lose! Scissors beats Paper';
+            return 'You lose this round! Scissors beats Paper';
         } else if ((playerSelection === 'paper' && computerSelection === 'paper')
                 || (playerSelection === 'rock' && computerSelection === 'rock')
                 || (playerSelection === 'scissors' && computerSelection === 'scissors')){
             return 'Wow, it\'s a tie!';
         } else {
-            return 'Sorry, Invalid Option!';  
+            return 'Sorry, Invalid Option!';
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+//console.log (playRound(playerSelection, computerSelection));
+
+//Below is a function to create a 5 round game and return the winner
+
+function game(){
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i<5; i++){
+        let computerSelection = getComputerChoice();
+        let playerSelection = getPlayerChoice();
+        let response = (playRound(playerSelection, computerSelection));
+        console.log(response);
+        //console.log(computerSelection);
+        //console.log(playerSelection);
+
+//Keep score
+
+        if ((response === 'You win this round! Rock beats Scissors')
+            || (response === 'You win this round! Scissors beats Paper')
+            || (response === 'You win this round! Paper beats Rock')){
+            playerScore = playerScore + 1;
+        } else if ((response === 'You lose this round! Rock beats Scissors')
+            || (response === 'You lose this round! Paper beats Rock')
+            || (response === "You lose this round! Scissors beats Paper")){
+            computerScore = computerScore + 1;
+        }
+    }
+            //console.log(playerScore);
+            //console.log(computerScore);
+
+    if (playerScore > computerScore){
+        console.log('Congrats, you\'re the big winner!');
+    } else if (playerScore < computerScore){
+        console.log('Sorry, you lose this game!');
+    } else if (playerScore === computerScore){
+        console.log('Somehow it\'s a tie!');
+    } else {
+        console.log('Something went wrong!');
+    }
+}
+
+
+game();
