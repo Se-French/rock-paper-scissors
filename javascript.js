4b56e7eb4a39ea9fd934e0255579fe3a5f5e9e51
Se-French <116462983+Se-French@users.noreply.github.com>
2023-01-07 16:17:18 -0500

    Remove comments and clean code
    Simplified else...if statement in playRound() to make code more concise. Edited else...if statment
    for response in game() to match the changes made to playRound().

--- javascript.js
+++ javascript.js
@@ -1,16 +1,10 @@
-//This function will randomly return 'rock', 'paper', or 'scissors'
-//Use this for computerSelection
-
 function getComputerChoice(){
     const options = ['rock', 'paper', 'scissors'];
-    let randomChoice = options[Math.floor(Math.random() * options.length)];
-    return randomChoice;
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 //let computerSelection = getComputerChoice();
-//console.log(computerSelection);
-
-//Below is where we get the value for playerSelection
+//console.log (computerSelection);
 
 function getPlayerChoice(){
     const playerChoice = prompt("Choose rock, paper, or scissors!");
@@ -29,28 +23,18 @@ function getPlayerChoice(){
     }
 
 //let playerSelection = getPlayerChoice();
-
 //console.log (playerSelection);
-
-//The following function plays a single round of the game and includes results
-//for all possible outcomes 
-
+ 
 function playRound(playerSelection, computerSelection){
-        if (playerSelection === 'rock' && computerSelection === 'scissors'){
-            return 'You win this round! Rock beats Scissors';
-        } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
-            return 'You win this round! Scissors beats Paper';
-        } else if (playerSelection === 'paper' && computerSelection === 'rock'){
-            return 'You win this round! Paper beats Rock';
-        } else if (playerSelection === 'scissors' && computerSelection === 'rock'){
-            return 'You lose this round! Rock beats Scissors';
-        } else if (playerSelection === 'rock' && computerSelection === 'paper'){
-            return 'You lose this round! Paper beats Rock';
-        } else if (playerSelection === 'paper' && computerSelection === 'scissors'){
-            return 'You lose this round! Scissors beats Paper';
-        } else if ((playerSelection === 'paper' && computerSelection === 'paper')
-                || (playerSelection === 'rock' && computerSelection === 'rock')
-                || (playerSelection === 'scissors' && computerSelection === 'scissors')){
+        if ((playerSelection === 'rock' && computerSelection === 'scissors')
+            ||(playerSelection === 'scissors' && computerSelection === 'paper')
+            ||(playerSelection === 'paper' && computerSelection === 'rock')){
+            return 'You win this round! ' + playerSelection + ' beats ' + computerSelection;
+        } else if ((playerSelection === 'scissors' && computerSelection === 'rock')
+            ||(playerSelection === 'rock' && computerSelection === 'paper')
+            ||(playerSelection === 'paper' && computerSelection === 'scissors')){
+            return 'You lose this round! ' + computerSelection + ' beats ' + playerSelection;
+        } else if (playerSelection === computerSelection){
             return 'Wow, it\'s a tie!';
         } else {
             return 'Sorry, Invalid Option!';
@@ -58,8 +42,6 @@ function playRound(playerSelection, computerSelection){
 }
 
 //console.log (playRound(playerSelection, computerSelection));
-
-//Below is a function to create a 5 round game and return the winner
 
 function game(){
 
@@ -73,28 +55,26 @@ function game(){
         console.log(response);
         //console.log(computerSelection);
         //console.log(playerSelection);
-
-//Keep score
-
-        if ((response === 'You win this round! Rock beats Scissors')
-            || (response === 'You win this round! Scissors beats Paper')
-            || (response === 'You win this round! Paper beats Rock')){
-            playerScore = playerScore + 1;
-        } else if ((response === 'You lose this round! Rock beats Scissors')
-            || (response === 'You lose this round! Paper beats Rock')
-            || (response === "You lose this round! Scissors beats Paper")){
-            computerScore = computerScore + 1;
-        }
+            //Keep score
+            if ((response === 'You win this round! rock beats scissors')
+                    || (response === 'You win this round! scissors beats paper')
+                    || (response === 'You win this round! paper beats rock')){
+                    playerScore = playerScore + 1;
+                } else if ((response === 'You lose this round! rock beats scissors')
+                    || (response === 'You lose this round! paper beats rock')
+                    || (response === "You lose this round! scissors beats paper")){
+                    computerScore = computerScore + 1;
+                }
     }
             //console.log(playerScore);
             //console.log(computerScore);
 
     if (playerScore > computerScore){
-        console.log('Congrats, you\'re the big winner!');
+        console.log('Congrats, you\'re the big game winner!');
     } else if (playerScore < computerScore){
         console.log('Sorry, you lose this game!');
     } else if (playerScore === computerScore){
-        console.log('Somehow it\'s a tie!');
+        console.log('This game is a tie!');
     } else {
         console.log('Something went wrong!');
     }
